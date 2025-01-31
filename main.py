3fe93da3376f0506354ae495049548fff787fc14
Mariano.Y <youngmariano99@gmail.com>
2025-01-31 19:45:24 -0300

    Solucioné la organizacion de los archivos

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), 'modules'))
+
 from gestion_stock import agregar_productos_inventario, mostrar_productos, quitar_productos_inventario, actualizar_stock
 from gestion_ventas import registrar_venta
 from menus import mostrar_menu_principal
